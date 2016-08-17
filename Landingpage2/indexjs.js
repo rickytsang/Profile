76a100f43761c8bf23c5e3482ea733000230a0ac
rickytsang <cuddlesching@gmail.com>
2016-08-17 18:37:38 +0800

    successful apply jquery

--- Landingpage2/indexjs.js
+++ Landingpage2/indexjs.js
@@ -1,5 +1,22 @@
-$(document).ready({
-	$('#prev').click(fucntion(){
-			$('#product3').show();
-	});
+$(document).ready(function(){
+    $("#prev").click(function(){
+	    if ($("#product1").css("display")!="none"){
+		    $(".jumbotron").css("background",'url("1080-3.jpeg")');
+		    $("#overlapimg").attr("src","400-3.jpeg");
+        $("#product1").css("display","none");
+        $("#product3").css("display","inline");
+        }
+        else if($("#product3").css("display")!="none"){
+	        $(".jumbotron").css("background",'url("1080-2.jpeg")');
+	        $("#overlapimg").attr("src","400-2.jpeg");
+	        $("#product3").css("display","none");
+					$("#product2").css("display","inline");
+        }
+        else{
+	        $("#product2").css("display","none");
+	        $(".jumbotron").css("background",'url("1080.jpeg")');
+	        $("#overlapimg").attr("src","400.jpeg");
+					$("#product1").css("display","inline");
+        }
+    });
 });
