3f55098e52326b1efb84b5a3d25002537e6fd7fb
rickytsang <cuddlesching@gmail.com>
2016-08-18 18:21:30 +0800

    change button

--- Landingpage2/indexjs.js
+++ Landingpage2/indexjs.js
@@ -19,4 +19,25 @@ $(document).ready(function(){
 					$("#product1").css("display","inline");
         }
     });
+    $("#next").click(function(){
+	    if ($("#product1").css("display")!="none"){
+		    $(".jumbotron").css("background",'url("1080-2.jpeg")');
+		    $("#overlapimg").attr("src","400-2.jpeg");
+        $("#product1").css("display","none");
+        $("#product2").css("display","inline");
+        }
+        else if($("#product3").css("display")!="none"){
+	        $(".jumbotron").css("background",'url("1080.jpeg")');
+	        $("#overlapimg").attr("src","400.jpeg");
+	        $("#product3").css("display","none");
+					$("#product1").css("display","inline");
+        }
+        else{
+	        $(".jumbotron").css("background",'url("1080-3.jpeg")');
+	        $("#overlapimg").attr("src","400-3.jpeg");
+	        $("#product2").css("display","none");
+					$("#product3").css("display","inline");
+        }
+    });
+
 });
